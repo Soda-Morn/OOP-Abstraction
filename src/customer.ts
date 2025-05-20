498aa4e6b0e6ebe9437457d77c0f935bc19c95a3
Morn Soda <mornsoda23@gmail.com>
2025-05-20 07:21:41 +0700

    add

--- src/customer.ts
+++ src/customer.ts
@@ -1,20 +1,11 @@
-export abstract class UserLogin {
-  protected username: string;
-  protected password: string;
+import { UserLogin } from "./UserLogin";
 
-  constructor(username: string, password: string) {
-    this.username = username;
-    this.password = password;
-  }
-
-  abstract login(): void;
-}
-
-class CustomerLogin extends UserLogin {
+export class Customer extends UserLogin {
   login(): void {
-    console.log(`Customer '${this.username}' logged in. Showing product catalog and order history.`);
+    console.log(`✅ customer ${this.firstname + this.lastname} logged in.`);
+  }
+
+  register(): void {
+    console.log(`📋 customer ${this.firstname + this.lastname} registered.`);
   }
 }
-
-const customer = new CustomerLogin("customerUser", "cust789");
-customer.login();
